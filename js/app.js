03eab7734d2d559a4e07c33fd582d42c9ddcffc1
tekthree <tekthree@gmail.com>
2020-12-09 21:02:43 -0800

    first feature finished

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strick'
+let allAnimalsinfo = [];
+
+function AnimalMaster(data){
+  this.pathUrl = data.image_url;
+  this.title = data.title;
+  this.description = data.description;
+  this.keyword = data.keyword;
+  this.horns = data.horns;
+
+  allAnimalsinfo.push(this);
+
+} 
+
+
+$.ajax('../data/page-1.json')
+  .then(data => {
+    data.forEach(value => {
+      let animal = new AnimalMaster(value);
+
+    });
+
+    allAnimalsinfo.forEach(value => {
+      let $clonePhotoTemp = $('#photo-template').clone();
+      $clonePhotoTemp.removeAttr('id');
+      $clonePhotoTemp.attr('class', value.keyword);
+      $clonePhotoTemp.find('h2').text(value.title);
+      $clonePhotoTemp.find('img').attr({'src': value.pathUrl, 'alt' : value.title });
+      $clonePhotoTemp.find('p').text(value.description);
+      $('main').append($clonePhotoTemp);
+
+
+    });
+
+
+  });
+
+
+  
+
